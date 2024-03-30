820c772ba4fb3caa3b2a8d5ffd10b85bb9a3a34d
SM-Heang <mheang67@gmail.com>
2024-03-30 10:25:03 +0700

    update responsive about page complete 100%

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,4 @@
+
 <template>
   <div class="">
     <div class="w-full">
@@ -5,84 +6,76 @@
         អំពីទស្សនាវដ្ដីស្រាវជ្រាវ
       </div>
       <div class="w-14 lg:w-20 border-b-2 lg:border-b-4 border-b-active_color mx-auto"></div>
-      <div class="flex bg-white rounded-lg justify-center items-center mt-10 py-10">
+      <div class="flex xs:mx-2 bg-white rounded-lg justify-center items-center mt-10 py-5 xl:flex-row flex-col">
         <div class="text-center mb-10  border-solid border-[1px] border-black-50 mx-10 p-5 rounded-xl">
-          <img src="../assets/img/logo/journal-logo.png" alt="journal logo" class="max-h-[300px] max-w-[900px]">
+          <img src="../assets/img/logo/journal-logo.png" alt="journal logo" class="2xl:max-h-[18.75rem] 2xl:max-w-[rem] lg:max-h-[15.625rem] lg:max-w-[18.75rem] md:max-h-[12.5rem] md:max-w-[15.625rem] sm:max-h-[12.5rem] sm:max-w-[15.625rem] xs:max-h-[12.5rem] xs:max-w-[15.625rem]">
           <p class="text-usea_primary font-hanuman text-2xl font-bold">និមិត្តសញ្ញា</p>
         </div>
         <div class="">
-          <p class="text-lg font-hanuman text-black px-10 text-justify">ទស្សនាវដ្ដីស្រាវជ្រាវសាកលវិទ្យាល័យសៅស៍អ៊ីសថ៍អេយសៀមានអក្សរកាត់ជាខេមរភាសា “”ទ.ស.ស.ស.អ.” និង មានឈ្មោះជាភាសាអង់គ្លេស <span class="font-extrabold">USEA Research Journal</span>ដែលមាន អក្សរកាត់“USEARJ”ទស្សនាវដ្តីស្រាវជ្រាវនេះគឺជា ទស្សនាវដ្តីស្រាវជ្រាវបោះពុម្ពផ្សាយតាមអនឡាញពីរដង ក្នុងមួយឆ្នាំជាខេមរភាសា និង/ឬភាសាអង់គ្លេស។ រាល់អត្ថបទ ស្រាវជ្រាវដែលបានបោះពុម្ពផ្សាយតាមទស្សនាវដ្ដីស្រាវជ្រាវសាកលវិទ្យាល័យ សៅស៍អ៊ីសថ៍អេយសៀ គឺមិនត្រូវបានគិតថ្លៃសេវាឡើយ ។</p>
+          <p class="2xl:text-lg xl:text-lg md:text-lg sm:text-[1rem] font-hanuman px-10 text-justify text-black ">ទស្សនាវដ្ដីស្រាវជ្រាវសាកលវិទ្យាល័យសៅស៍អ៊ីសថ៍អេយសៀមានអក្សរកាត់ជាខេមរភាសា “ទ.ស.ស.ស.អ.” និង មានឈ្មោះជាភាសាអង់គ្លេស <span class="font-extrabold">USEA Research Journal</span>ដែលមាន អក្សរកាត់“USEARJ”ទស្សនាវដ្តីស្រាវជ្រាវនេះគឺជា ទស្សនាវដ្តីស្រាវជ្រាវបោះពុម្ពផ្សាយតាមអនឡាញពីរដង ក្នុងមួយឆ្នាំជាខេមរភាសា និង/ឬភាសាអង់គ្លេស។ រាល់អត្ថបទ ស្រាវជ្រាវដែលបានបោះពុម្ពផ្សាយតាមទស្សនាវដ្ដីស្រាវជ្រាវសាកលវិទ្យាល័យ សៅស៍អ៊ីសថ៍អេយសៀ គឺមិនត្រូវបានគិតថ្លៃសេវាឡើយ ។</p>
         </div>
       </div>
     </div>
     <div class="mt-5"></div>
     <!-- start journal vision mission -->
-    <div class="relative mb-3">
-      <h6 class="mb-0">
-        <button
-          class="bg-white relative flex items-center w-full p-4 font-semibold text-left transition-all ease-in border-b border-solid cursor-pointer border-slate-100 text-slate-700 rounded-t-1 group text-dark-500 rounded-lg"
-          data-collapse-target="collapse-1"
-        >
-          <span class="font-hanuman text-2xl text-usea_primary">គោលបំណង</span>
-          <i class="px-5 absolute right-0 pt-1 text-xs fa fa-plus group-open:opacity-0"></i>
-          <i class="absolute right-0 pt-1 text-xs opacity-0 fa fa-minus group-open:opacity-100"></i>
+    <div>
+      <!-- First Accordion -->
+      <div class="py-2 xs:mx-2 bg-white rounded-lg">
+        <button class="flex p-3 justify-between w-full" @click="toggleAnswer(1)">
+          <span class="text-usea_primary 2xl:text-2xl md:text-xl sm:text-xl xs:text-xl font-extrabold font-hanuman">គោលបំណង</span>
+          <span class="text-2xl" v-if="showAnswer === 1">-</span>
+          <span class="text-2xl" v-else>+</span>
         </button>
-      </h6>
-      <div
-        data-collapse="collapse-1"
-        class="h-0 overflow-hidden transition-all duration-300 ease-in-out"
-      >
-        <div class="p-4 text-sm leading-normal text-blue-gray-500/80">
-          We're not always in the position that we want to be at. We're constantly
-          growing. We're constantly making mistakes. We're constantly trying to
-          express ourselves and actualize our dreams.
+        <div class="" v-if="showAnswer === 1">
+          <div class="p-3 font-hanuman 2xl:text-lg xl:text-lg md:text-lg sm:text-[1rem]">ដើម្បីចូលរួមចំណែកលើកកម្ពស់ការស្រាវជ្រាវនៅសាកលវិទ្យាល័យសៅស៍អ៊ីសថ៍អេយសៀនិងនៅក្នុងព្រះរាជាណាចក្រកម្ពុជាតាមរយៈការបោះពុម្ពផ្សាយអត្ថបទស្រាវជ្រាវសម្រាប់បង្កើនធនធានសិក្សាដែលគាំទ្រដល់ការសិក្សា និងការស្រាវជ្រាវ បែបវិទ្យាសាស្រ្ត ។</div>
         </div>
       </div>
-    </div>
-    <div class="relative mb-3">
-      <h6 class="mb-0">
-        <button
-          class="bg-white relative flex items-center w-full p-4 font-semibold text-left transition-all ease-in border-b border-solid cursor-pointer border-slate-100 text-slate-700 rounded-t-1 group text-dark-500 rounded-lg"
-          data-collapse-target="collapse-2"
-        >
-        <span class="font-hanuman text-2xl text-usea_primary">វិសាលភាព</span>
-          <i class="px-5 absolute right-0 pt-1 text-xs fa fa-plus group-open:opacity-0"></i>
-          <i class="absolute right-0 pt-1 text-xs opacity-0 fa fa-minus group-open:opacity-100"></i>
+    
+    <!-- Second Accordion -->
+      <div class="py-2 xs:mx-2 bg-white rounded-lg mt-3">
+        <button class="flex p-3 justify-between w-full" @click="toggleAnswer(2)">
+          <span class="text-usea_primary 2xl:text-2xl md:text-xl sm:text-xl xs:text-xl font-extrabold font-hanuman">វិសាលភាព</span>
+          <span class="text-2xl" v-if="showAnswer === 2">-</span>
+          <span class="text-2xl" v-else>+</span>
         </button>
-      </h6>
-      <div
-        data-collapse="collapse-2"
-        class="h-0 overflow-hidden transition-all duration-300 ease-in-out"
-      >
-        <div class="p-4 text-sm leading-normal text-blue-gray-500/80">
-          We're not always in the position that we want to be at. We're constantly
-          growing. We're constantly making mistakes. We're constantly trying to
-          express ourselves and actualize our dreams.
+        <div v-if="showAnswer === 2">
+          <div class="p-3 font-hanuman 2xl:text-lg xl:text-lg md:text-lg sm:text-[1rem]">ទស្សនាវដ្ដីស្រាវជ្រាវសាកលវិទ្យាល័យ សៅស៍អ៊ីសថ៍អេយសៀ ជាទស្សនាវដ្តីស្រាវជ្រាវកម្រិតជាតិ ដែលបោះពុម្ពផ្សាយអត្ថបទស្រាវជ្រាវក្នុងវិស័យ៖ <ul><li>‐	សេដ្ឋកិច្ច </li><li>‐	ធុរកិច្ច</li><li>‐	ទេសចរណ៍ </li><li>‐	ច្បាប់ </li><li>‐	សិល្បៈ និងអប់រំ</li><li>‐	វិទ្យាសាស្រ្ត និងបច្ចេកវិទ្យាឌីជីថល និង</li><li>‐	បរិស្ថាន ។</li></ul> ទស្សនាវដ្តីនេះ ទទួលបោះពុម្ពផ្សាយអត្ថបទថ្មីៗ ដែលមិនទាន់បានបោះពុម្ពនៅកន្លែងណាផ្សេង ទោះជាក្នុងភាសាអ្វីក៏ដោយ រួមមានអត្ថបទស្រាវជ្រាវ (original articles) វិវេចនាអត្ថបទ (review articles) និងអត្ថបទសង្ខេបគោលនយោបាយ (policy briefs) ។ </div>
         </div>
       </div>
-    </div>
-    <div class="relative mb-3">
-      <h6 class="mb-0">
-        <button
-          class="bg-white relative flex items-center w-full p-4 font-semibold text-left transition-all ease-in border-b border-solid cursor-pointer border-slate-100 text-slate-700 rounded-t-1 group text-dark-500 rounded-lg"
-          data-collapse-target="collapse-3"
-        >
-        <span class="font-hanuman text-2xl text-usea_primary">អង្គភាពចេញផ្សាយ</span>
-          <i class="px-5 absolute right-0 pt-1 text-xs fa fa-plus group-open:opacity-0"></i>
-          <i class="absolute right-0 pt-1 text-xs opacity-0 fa fa-minus group-open:opacity-100"></i>
+    
+    <!-- Third Accordion -->
+      <div class="py-2 xs:mx-2 bg-white rounded-lg mt-3">
+        <button class="flex p-3 justify-between w-full" @click="toggleAnswer(3)">
+          <span class="text-usea_primary 2xl:text-2xl md:text-xl sm:text-xl xs:text-xl font-extrabold font-hanuman">អង្គភាពចេញផ្សាយ</span>
+          <span class="text-2xl" v-if="showAnswer === 3">-</span>
+          <span class="text-2xl" v-else>+</span>
         </button>
-      </h6>
-      <div
-        data-collapse="collapse-3"
-        class="h-0 overflow-hidden transition-all duration-300 ease-in-out"
-      >
-        <div class="p-4 text-sm leading-normal text-blue-gray-500/80">
-          We're not always in the position that we want to be at. We're constantly
-          growing. We're constantly making mistakes. We're constantly trying to
-          express ourselves and actualize our dreams.
+        <div v-if="showAnswer === 3">
+          <div class="p-3 font-hanuman 2xl:text-lg xl:text-lg md:text-lg sm:text-[1rem]">ចេញផ្សាយដោយ៖ សាលាក្រោយបរិញ្ញាបត្រនៃសាកលវិទ្យាល័យសៅស៍អ៊ីសថ៍អេយសៀ។</div>
         </div>
       </div>
-    </div>
+  </div>
+  
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      showAnswer: null
+    };
+  },
+  methods: {
+    toggleAnswer(index) {
+      if (this.showAnswer === index) {
+        // If the clicked accordion is already open, close it
+        this.showAnswer = null;
+      } else {
+        // Close the previously opened accordion (if any)
+        this.showAnswer = index;
+      }
+    }
+  }
+};
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,24 @@ export default {
       Khmer_OS_Battambang: ["Khmer_OS_Battambang", "sans-serif"],
       Khmer_OS_Muol_Light: ["Khmer_OS_Muol_Light", "sans-serif"],
     },
+    screens: {
+      'xs': '320px',
+      // => @media (min-width: 320px) { ... }
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 };
