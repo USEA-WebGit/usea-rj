d198469a67d5901435180fb5d20b769835285f4c
SM-Heang <mheang67@gmail.com>
2024-04-02 10:25:35 +0700

    update text

--- src/views/instructions/Instruction-Authors.vue
+++ src/views/instructions/Instruction-Authors.vue
@@ -1,13 +1,57 @@
 <template>
     <div class="">
-      <div class="w-full">
-        <div
-          class="w-fit mx-auto text-active_color text-center font-hanuman font-bold text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl"
-        >
-          សេចក្ដីណែនាំអ្នកនិពន្ធ
+        <div class="w-full">
+            <div class="w-fit mx-auto text-active_color text-center font-hanuman font-bold text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl">
+            គោលការណ៍នៃការបោះពុម្ភ
+            </div>
+            <div class="w-14 lg:w-20 border-b-2 lg:border-b-4 border-b-active_color mx-auto"></div>
+            <div class="flex xs:mx-2 rounded-lg justify-center items-center py-5 xl:flex-row flex-col">
+                <div class="">
+                    <ol class="2xl:text-lg xl:text-lg md:text-lg sm:text-[1rem] font-hanuman px-10 text-justify text-black list-decimal">
+                        <li>បោះពុម្ពតាមប្រព័ន្ធអនឡាញក្នុងគេហទំព័រផ្លូវការរបស់ទស្សនាវដ្តីស្រាវជ្រាវសាកលវិទ្យាល័យ សៅស៍អ៊ីសថ៍អេយសៀ</li>
+                        <li>បោះពុម្ពតាមបែបបើកចំហ (Open Access)</li>
+                        <li>ទទួលយកអត្ថបទពេញមួយឆ្នាំដើម្បីពិនិត្យមុនបោះពុម្ព និងធ្វើការបោះពុម្ព ២លេខ ជារៀងរាល់ឆ្នាំ។ ការបោះពុម្ពនឹងធ្វើឡើងនៅខែមិថុនា និងខែធ្នូ នៃឆ្នាំនីមួយៗ</li>
+                        <li>អត្ថបទបោះពុម្ពផ្សាយត្រូវឆ្លងកាត់ការត្រួតពិនិត្យពីអ្នកជំនាញ</li>
+                        <li>ទស្សនាវដ្ដីស្រាវជ្រាវនេះ អនុញ្ញាតឱ្យអ្នកនិពន្ធមានសិទ្ធិក្នុងការប្រើប្រាស់ឡើងវិញ ឬថតចម្លងអត្ថបទជាដើម តែត្រូវផ្តល់សិទ្ធិដល់ ទ.ស.ស.ស.អ. ពេលអត្ថបទខ្លួនត្រូវបានទទួលឱ្យបោះពុម្ព។</li>
+                        <li>រាល់ព័ត៌មានផ្ទាល់ខ្លួនដូចជាឈ្មោះអ្នកនិពន្ធ អ៊ីមែល ទ.ស.ស.ស.អ. នឹងរក្សាទុកប្រើប្រាស់សម្រាប់តែទស្សនាវដ្តីស្រាវជ្រាវនេះតែប៉ុណ្ណោះ។ ទ.ស.ស.ស.អ. នឹងមិនប្រើប្រាស់ព័ត៌មានទាំងនេះសម្រាប់គោលបំណងផ្សេង និងចែកចាយជាមួយភាគីទីបីឡើយ។ មានតែអ៊ីមែលអ្នកនិពន្ធឆ្លើយឆ្លងប៉ុណ្ណោះដែលត្រូវបង្ហាញនៅក្នុងអត្ថបទពេលបោះពុម្ពផ្សាយ។</li>
+                    </ol>
+                </div>
+            </div>
+            <div class="mt-10"></div>
+            <div class="w-fit mx-auto text-active_color text-center font-hanuman font-bold text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl">
+            ប្រភេទអត្ថបទ
+            </div>
+            <div class="w-14 lg:w-20 border-b-2 lg:border-b-4 border-b-active_color mx-auto"></div>
+            <div class="flex xs:mx-2 rounded-lg justify-center items-center py-5 xl:flex-row flex-col">
+                <div class="">
+                    <p class="2xl:text-lg xl:text-lg md:text-lg sm:text-[1rem] font-hanuman px-10 text-justify text-black">អត្ថបទដែលបញ្ជូនមក ទ.ស.ស.ស.អ. អាចមានបីប្រភេទ ដូចខាងក្រោម៖</p>
+                    <ul class="2xl:text-lg xl:text-lg md:text-lg sm:text-[1rem] font-hanuman px-10 text-justify text-black list-disc">
+                        <li><a href="#" class="font-bold underline">អត្ថបទស្រាវជ្រាវ (Original Articles)៖</a> ជាប្រភេទអត្ថបទស្រាវជ្រាវស្តីពី លទ្ធផលនៃការស្រាវជ្រាវដើម មានលក្ខណៈពេញលេញដូចជាសេចក្តីផ្តើមការរំលឹកទ្រឹស្តី វិធីសាស្រ្តស្រាវជ្រាវ លទ្ធផល ការពិភាក្សា និងសេចក្តីសន្និដ្ឋាន ជាដើម។ អត្ថបទស្រាវជ្រាវ មានចំនួនទំព័រសរុបមិនឱ្យលើសពី ២០ទំព័រ ។</li>
+                        <li><a href="#" class="font-bold underline">វិវេចនាអត្ថបទ (Review Articles)៖</a> ជាប្រភេទអត្ថបទស្រាវជ្រាវដែលសំយោគ ឬសង្ខេបពីការសិក្សាស្រាវជ្រាវផ្សេងៗលើប្រធានបទជាក់លាក់ណាមួយដែលជាចំណាប់អារម្មណ៍របស់អ្នកនិពន្ធ។ អត្ថបទប្រភេទនេះ មានបីប្រភេទ៖ វិវេចនាទ្រឹស្តី(literature reviews) វិវេចនាជាប្រព័ន្ធ(systematic reviews) និងការវិភាគមេតា(meta-analyses)។ វិវេចនាអត្ថបទ ជាទូទៅដកស្រង់ប្រហែល ១០០អត្ថបទស្រាវជ្រាវ ហើយមានលក្ខណៈពេញលេញដូចជាសេចក្តីផ្តើម ការពិភាក្សា និងសេចក្តីសន្និដ្ឋានជាដើម និងសរសេរមិនគួរឱ្យលើសពី ២០ទំព័រ ។</li>
+                        <li><a href="#" class="font-bold underline">ខ្លឹមសារសង្ខេបគោលនយោបាយ (Policy Briefs)៖</a> អត្ថបទប្រភេទនេះជា   អត្ថបទវិភាគ គោលនយោបាយអប់រំដែលមានការឆ្លុះបញ្ចាំងពីឧត្តមានុវត្តន៍ និងបញ្ហាប្រឈមនានាក្នុងការអនុវត្តគោលនយោបាយ។ ខ្លឹមសារសង្ខេបគោលនយោបាយ មានចំនួនទំព័រសរុបមិនឱ្យលើសពី ១៥ទំព័រ។</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="w-fit mx-auto text-active_color text-center font-hanuman font-bold text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl">
+            កម្មវិធីប្រើប្រាស់សម្រាប់រៀបចំអត្ថបទ
+            </div>
+            <div class="w-14 lg:w-20 border-b-2 lg:border-b-4 border-b-active_color mx-auto"></div>
+            <div class="flex xs:mx-2 rounded-lg justify-center items-center py-5 xl:flex-row flex-col">
+                <div class="text-center mb-10 mx-10 p-5 rounded-xl">
+                    <img src="../../assets/img/logo/latexnword.png" alt="policy image" class="2xl:max-h-[18.75rem] 2xl:max-w-[rem] lg:max-h-[15.625rem] lg:max-w-[18.75rem] md:max-h-[12.5rem] md:max-w-[15.625rem] sm:max-h-[12.5rem] sm:max-w-[15.625rem] xs:max-h-[12.5rem] xs:max-w-[15.625rem]">
+                    <!-- <p class="text-usea_primary font-hanuman text-2xl font-bold">និមិត្តសញ្ញា</p> -->
+                </div>
+                <div class="">
+
+                    <p class="2xl:text-lg xl:text-lg md:text-lg sm:text-[1rem] font-hanuman px-10 text-justify text-black">អ្នកនិពន្ធអាចប្រើប្រាស់កម្មវិធី Microsoft Word ដើម្បីរៀបចំអត្ថបទដែលត្រូវបញ្ជូនមកកាន់ ទ.ស.ស.ស.អ. ឬកម្មវិធី LaTex សម្រាប់រៀបចំអត្ថបទដែលមានរូបមន្តច្រើនដូចជាគណិតវិទ្យា និងស្ថិតិ ជាដើម ។</p>
+                    <!-- <ul class="2xl:text-lg xl:text-lg md:text-lg sm:text-[1rem] font-hanuman px-10 text-justify text-black list-disc">
+                        <li><a href="#" class="font-bold underline">អត្ថបទស្រាវជ្រាវ (Original Articles)៖</a> ជាប្រភេទអត្ថបទស្រាវជ្រាវស្តីពី លទ្ធផលនៃការស្រាវជ្រាវដើម មានលក្ខណៈពេញលេញដូចជាសេចក្តីផ្តើមការរំលឹកទ្រឹស្តី វិធីសាស្រ្តស្រាវជ្រាវ លទ្ធផល ការពិភាក្សា និងសេចក្តីសន្និដ្ឋាន ជាដើម។ អត្ថបទស្រាវជ្រាវ មានចំនួនទំព័រសរុបមិនឱ្យលើសពី ២០ទំព័រ ។</li>
+                        <li><a href="#" class="font-bold underline">វិវេចនាអត្ថបទ (Review Articles)៖</a> ជាប្រភេទអត្ថបទស្រាវជ្រាវដែលសំយោគ ឬសង្ខេបពីការសិក្សាស្រាវជ្រាវផ្សេងៗលើប្រធានបទជាក់លាក់ណាមួយដែលជាចំណាប់អារម្មណ៍របស់អ្នកនិពន្ធ។ អត្ថបទប្រភេទនេះ មានបីប្រភេទ៖ វិវេចនាទ្រឹស្តី(literature reviews) វិវេចនាជាប្រព័ន្ធ(systematic reviews) និងការវិភាគមេតា(meta-analyses)។ វិវេចនាអត្ថបទ ជាទូទៅដកស្រង់ប្រហែល ១០០អត្ថបទស្រាវជ្រាវ ហើយមានលក្ខណៈពេញលេញដូចជាសេចក្តីផ្តើម ការពិភាក្សា និងសេចក្តីសន្និដ្ឋានជាដើម និងសរសេរមិនគួរឱ្យលើសពី ២០ទំព័រ ។</li>
+                        <li><a href="#" class="font-bold underline">ខ្លឹមសារសង្ខេបគោលនយោបាយ (Policy Briefs)៖</a> អត្ថបទប្រភេទនេះជា   អត្ថបទវិភាគ គោលនយោបាយអប់រំដែលមានការឆ្លុះបញ្ចាំងពីឧត្តមានុវត្តន៍ និងបញ្ហាប្រឈមនានាក្នុងការអនុវត្តគោលនយោបាយ។ ខ្លឹមសារសង្ខេបគោលនយោបាយ មានចំនួនទំព័រសរុបមិនឱ្យលើសពី ១៥ទំព័រ។</li>
+                    </ul> -->
+                </div>
+            </div>
         </div>
-        <div class="w-14 lg:w-20 border-b-2 lg:border-b-4 border-b-active_color mx-auto"></div>
-      </div>
       <div class="mt-10"></div>
   
       <!-- end wellcome -->
